91c5bb84fdbb590f60a95d50f1820a01dafd3df1
wonchuz <120187423+wonchuz@users.noreply.github.com>
2023-02-27 22:37:02 +0800

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,8 @@
+public class Cell {
+    public final char START = 'S';
+    public final char GOAL = 'G';
+    public final char WALL = '#';
+    public final char EMPTY = '.';
+    public final char PATH = '-';
+    public final char EXPLORED = 'E';
+}
