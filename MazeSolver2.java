fa26f54569f56e31ceed01794202a902326c9d45
wonchuz <120187423+wonchuz@users.noreply.github.com>
2023-02-27 22:49:10 +0800

    Update MazeSolver2.java

--- MazeSolver2.java
+++ MazeSolver2.java
@@ -1,6 +1,6 @@
 import java.io.File;
 
-public class MazeSolver {
+public class MazeSolver2 {
     public static void main(String[] args) throws Exception {
         File input = new File("maze.txt");
         Maze maze = new Maze(input);
@@ -9,7 +9,7 @@ public class MazeSolver {
         int row = maze.start_coordinates[0];
         int col = maze.start_coordinates[1];
         
-        while(maze.travelDown(row, col) == true) {
+        while(maze.travel(row, col) == true) {
             row++;
         }
 
