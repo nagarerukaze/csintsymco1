652df82215e72e937df1c11c435fb2158cecde19
aaronjardenil <aaron_randall_jardenil@dlsu.edu.ph>
2023-03-05 17:56:57 +0800

    merged animation branch to main

--- Cell.java
+++
@@ -1,8 +0,0 @@
-public final class Cell {
-    public final char START = 'S';
-    public final char GOAL = 'G';
-    public final char WALL = '#';
-    public final char EMPTY = '.';
-    public final char PATH = '-';
-    public final char EXPLORED = 'E';
-}
--- Maze.java
+++ Maze.java
@@ -11,7 +11,7 @@ public class Maze {
     public Maze() {
         Scanner sc;
         try {
-            sc = new Scanner(new File("blocked_tc.txt"));
+            sc = new Scanner(new File("maze.txt"));
             this.n = sc.nextInt();
             this.maze = new char[n][n];
 
