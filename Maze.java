89e38dee7be1ed284475c2918527541bf74b5498
wonchuz <120187423+wonchuz@users.noreply.github.com>
2023-03-06 21:20:23 +0800

    .

--- Maze.java
+++ Maze.java
@@ -1,6 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Maze {
     private int n;
@@ -9,18 +9,20 @@ public class Maze {
     private int[] goal_coordinates = new int[2];
 
     public Maze() {
-        Scanner sc;
-        try {
-            sc = new Scanner(new File("maze.txt"));
-            this.n = sc.nextInt();
+        BufferedReader brReader;
+
+        //try {
+            //brReader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/resources/" + "maze.txt")));
+    
+            this.n = 3;//brReader.read();
             this.maze = new char[n][n];
 
             for(int i = 0; i < n; i++) {
-                char[] str = sc.next().toCharArray();
+                //char[] str = brReader.readLine().toCharArray();
 
                 for(int j = 0; j < n; j++) {
-                    this.maze[i][j] = str[j];
-
+                    this.maze[i][j] = '.';//str[j];
+                    /*
                     if(str[j] == 'S') {
                         this.start_coordinates[0] = i;
                         this.start_coordinates[1] = j;
@@ -28,12 +30,14 @@ public class Maze {
                         this.goal_coordinates[0] = i;
                         this.goal_coordinates[1] = j;
                     }
+                    */
                 }
             }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } 
+            //brReader.close();
+        //} catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+        //}
     }
 
     public int getN() {
